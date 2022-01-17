96898ecf122cdc087e27c40c73b898925393c833
amanpandey0286 <amanpandey.2020@vitbhopal.ac.in>
2022-01-17 19:24:05 +0530

    first commit

---
+++ lib/story.dart
@@ -0,0 +1,11 @@
+import 'package:destini_challenge_starting/story_brain.dart';
+
+class Story {
+  String storyTitle;
+
+  String choice1;
+
+  String choice2;
+
+  Story({this.storyTitle, this.choice1, this.choice2});
+}
